3a443ba947c08c9e054173028e3ea1aca21cb9f3
AminaChakirDev <amina.aitm@gmail.com>
2020-05-10 16:12:21 +0200

    ajout message d'erreur et extension autorisée OK

--- upload.php
+++ upload.php
@@ -1,18 +1,23 @@
 <?php
+
 if(isset($_POST['submit'])){
     if (count($_FILES['movies']['name']) > 0) {
         for ($i = 0; $i < count($_FILES['movies']['name']); $i++) {
             $tmpFilePath = $_FILES['movies']['tmp_name'][$i];
-            if(($_FILES['movies']['size'][$i]) < 1000000) {
-                if (($_FILES['movies']['type'][$i]) == 'image/png' || 'image/jpeg' || 'image/gif') {
-                    if ($tmpFilePath != "") {
-                        $uploadDir = 'uploads/';
-                        $extension = pathinfo($_FILES['movies']['name'][$i], PATHINFO_EXTENSION);
-                        $filename = uniqid() . '.' . $extension;
-                        $uploadFile = $uploadDir . $filename;
-                        move_uploaded_file($tmpFilePath, $uploadFile);
-                    }
-                }
+            if (($_FILES['movies']['size'][$i]) > 1000000) {
+                $sizeErr = "Fichier trop lourd";
+            }
+            if ($_FILES['movies']['type'][$i] !== 'image/jpeg'
+                AND $_FILES['movies']['type'][$i] !== 'image/png'
+                AND $_FILES['movies']['type'][$i] !== 'image/gif') {
+              $extErr = "Extension non autorisée";
+            }
+            elseif ($tmpFilePath != "") {
+                            $uploadDir = 'uploads/';
+                            $extension = pathinfo($_FILES['movies']['name'][$i], PATHINFO_EXTENSION);
+                            $filename = uniqid() . '.' . $extension;
+                            $uploadFile = $uploadDir . $filename;
+                            move_uploaded_file($tmpFilePath, $uploadFile);
             }
         }
     }
@@ -35,18 +40,18 @@ if(isset($_POST['submit'])){
     <body>
         <form action="" method="post" enctype="multipart/form-data">
             <label for="imageUpload">Upload an profile image</label>
-            <input type="file" name="movies[]" multiple="multiple" />
-            <input type="file" name="movies[]" multiple="multiple" />
+            <div><?php if (isset($sizeErr)) { echo $sizeErr;} ?></div>
+            <span><?php if (isset($extErr)) { echo $extErr;} ?></span>
             <input type="file" name="movies[]" multiple="multiple" />
             <p><input type="submit" name="submit" value="Submit"></p>
         </form>
         <?php
         $it = new FilesystemIterator(__DIR__.'/uploads');
         foreach ($it as $fileinfo) {
-            echo "<li><figure>";
+            echo "<figure>";
             echo "<img src=\"" . '/uploads/' . $fileinfo->getFilename() . "\">";
             echo "<figcaption>" . $fileinfo->getFilename() . "</figcaption>";
-            echo "</figure></li><br>";
+            echo "</figure><br>";
         }
         ?>
     </body>
